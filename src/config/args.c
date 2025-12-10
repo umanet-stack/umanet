a6a189a266f5b4d3a8dd47761eff26c3951c6ad7
Idhibhat Pankam <marginpankam@gmail.com>
2025-12-10 03:55:53 -0700

    new args

--- src/config/args.c
+++ src/config/args.c
@@ -46,6 +46,86 @@ config_t config = {
 
 #define CFG config
 
+enum cfg_params {
+    CP_PORTMASK,
+    CP_PROMISCIOUS,
+    CP_VM2VM,
+    CP_RX_RETRY,
+    CP_RX_RETRY_DELAY,
+    CP_RX_RETRY_NUM,
+    CP_MERGEABLE,
+    CP_STATS,
+    CP_SOCKET_FILE,
+    CP_TX_CSUM,
+    CP_TSO,
+    CP_CLIENT,
+    CP_DEQUEUE_ZERO_COPY,
+    CP_BUILTIN_NET_DRIVER,
+};
+
+static struct option options[] = {
+    {
+        "portmask",
+        required_argument,
+        .val = CP_PORTMASK,
+    },
+    {
+        "promiscuous",
+        no_argument,
+        .val = CP_PROMISCIOUS,
+    },
+    {
+        "vm2vm",
+        required_argument,
+        .val = CP_VM2VM,
+    },
+    {
+        "rx-retry",
+        required_argument,
+        .val = CP_RX_RETRY,
+    },
+    {
+        "rx-retry-delay",
+        required_argument,
+        .val = CP_RX_RETRY_DELAY,
+    },
+    {
+        "rx-retry-num",
+        required_argument,
+        .val = CP_RX_RETRY_NUM,
+    },
+    {
+        "mergeable",
+        required_argument,
+        .val = CP_MERGEABLE,
+    },
+    {
+        "stats",
+        required_argument,
+        .val = CP_STATS,
+    },
+    {
+        "socket-file",
+        required_argument,
+        .val = CP_SOCKET_FILE,
+    },
+    {
+        "tx-csum",
+        required_argument,
+        .val = CP_TX_CSUM,
+    },
+    {
+        "tso",
+        required_argument,
+        .val = CP_TSO,
+    },
+    {"client", no_argument, &CFG.client_mode, 1},
+    {"dequeue-zero-copy", no_argument, &CFG.dequeue_zero_copy, 1},
+    {"builtin-net-driver", no_argument, &CFG.builtin_net_driver, 1},
+    // {"dequeue-zero-copy", no_argument, &CFG.dequeue_zero_copy, 1},
+    // mean if there's --dequeue-zero-copy, then CFG.dequeue_zero_copy = 1, else 0
+};
+
 /*
  * Parse the portmask provided at run time.
  */
@@ -149,158 +229,126 @@ int us_vhost_parse_args(int argc, char **argv) {
     int option_index;
     unsigned i;
     const char *prgname = argv[0];
-    static struct option long_option[] = {
-        {"vm2vm", required_argument, NULL, 0},
-        {"rx-retry", required_argument, NULL, 0},
-        {"rx-retry-delay", required_argument, NULL, 0},
-        {"rx-retry-num", required_argument, NULL, 0},
-        {"mergeable", required_argument, NULL, 0},
-        {"stats", required_argument, NULL, 0},
-        {"socket-file", required_argument, NULL, 0},
-        {"tx-csum", required_argument, NULL, 0},
-        {"tso", required_argument, NULL, 0},
-        {"client", no_argument, &CFG.client_mode, 1},
-        {"dequeue-zero-copy", no_argument, &CFG.dequeue_zero_copy, 1},
-        {"builtin-net-driver", no_argument, &CFG.builtin_net_driver, 1},
-        {NULL, 0, 0, 0},
-    };
 
-    /* Parse command line */
-    while ((opt = getopt_long(argc, argv, "p:P", long_option, &option_index)) != EOF) {
+    while ((opt = getopt_long(argc, argv, "", options, NULL)) != EOF) {
         switch (opt) {
-        /* Portmask */
-        case 'p':
+        case CP_PORTMASK:
             CFG.enable_port_mask = parse_portmask(optarg);
             if (CFG.enable_port_mask == 0) {
-                RTE_LOG(INFO, VHOST_CONFIG, "Invalid portmask\n");
+                fprintf(stderr, "Invalid portmask\n");
+                goto failed;
+            }
+            break;
+
+        case CP_PROMISCIOUS:
+            CFG.promiscuous = 1;
+            CFG.vmdq_conf_default->rx_adv_conf.vmdq_rx_conf.rx_mode =
+                ETH_VMDQ_ACCEPT_BROADCAST | ETH_VMDQ_ACCEPT_MULTICAST;
+            break;
+
+        case CP_VM2VM:
+            /* Enable/disable vm2vm comms. */
+            ret = parse_num_opt(optarg, (VM2VM_LAST - 1));
+            if (ret == -1) {
+                fprintf(stderr, "Invalid argument for vm2vm [0|1|2]\n");
+                goto failed;
+            } else {
+                CFG.vm2vm_mode = (vm2vm_type)ret;
+            }
+            break;
+
+        case CP_RX_RETRY:
+            /* Enable/disable retries on RX. */
+            ret = parse_num_opt(optarg, 1);
+            if (ret == -1) {
+                fprintf(stderr, "Invalid argument for rx-retry [0|1]\n");
+                goto failed;
+            } else {
+                CFG.enable_retry = ret;
+            }
+            break;
+        case CP_TX_CSUM:
+            /* Enable/disable TX checksum offload. */
+            ret = parse_num_opt(optarg, 1);
+            if (ret == -1) {
+                fprintf(stderr, "Invalid argument for tx-csum [0|1]\n");
+                goto failed;
+            } else
+                CFG.enable_tx_csum = ret;
+            break;
+
+        case CP_TSO:
+            /* Enable/disable TSO offload. */
+            ret = parse_num_opt(optarg, 1);
+            if (ret == -1) {
+                fprintf(stderr, "Invalid argument for tso [0|1]\n");
+                goto failed;
+            } else
+                CFG.enable_tso = ret;
+            break;
+
+        case CP_RX_RETRY_DELAY:
+            /* Specify the retries delay time (in useconds) on RX. */
+            ret = parse_num_opt(optarg, INT32_MAX);
+            if (ret == -1) {
+                fprintf(stderr, "Invalid argument for rx-retry-delay [0-N]\n");
+                goto failed;
+            } else {
+                CFG.burst_rx_delay_time = ret;
+            }
+            break;
+
+        case CP_RX_RETRY_NUM:
+            /* Specify the retries number on RX. */
+            ret = parse_num_opt(optarg, INT32_MAX);
+            if (ret == -1) {
+                fprintf(stderr, "Invalid argument for rx-retry-num [0-N]\n");
+                goto failed;
+            } else {
+                CFG.burst_rx_retry_num = ret;
+            }
+            break;
+
+        case CP_MERGEABLE:
+            /* Enable/disable RX mergeable buffers. */
+            ret = parse_num_opt(optarg, 1);
+            if (ret == -1) {
+                RTE_LOG(INFO, VHOST_CONFIG, "Invalid argument for mergeable [0|1]\n");
+                us_vhost_usage(prgname);
+                return -1;
+            } else {
+                CFG.mergeable = !!ret;
+                if (ret) {
+                    CFG.vmdq_conf_default->rxmode.offloads |= DEV_RX_OFFLOAD_JUMBO_FRAME;
+                    CFG.vmdq_conf_default->rxmode.max_rx_pkt_len = JUMBO_FRAME_MAX_SIZE;
+                }
+            }
+            break;
+
+        case CP_STATS:
+            /* Enable/disable stats. */
+            ret = parse_num_opt(optarg, INT32_MAX);
+            if (ret == -1) {
+                RTE_LOG(INFO, VHOST_CONFIG, "Invalid argument for stats [0..N]\n");
+                us_vhost_usage(prgname);
+                return -1;
+            } else {
+                CFG.enable_stats = ret;
+            }
+            break;
+
+        case CP_SOCKET_FILE:
+            /* Set socket file path. */
+            if (us_vhost_parse_socket_path(optarg) == -1) {
+                RTE_LOG(INFO, VHOST_CONFIG, "Invalid argument for socket name (Max %d characters)\n", PATH_MAX);
                 us_vhost_usage(prgname);
                 return -1;
             }
             break;
 
-        case 'P':
-            CFG.promiscuous = 1;
-            CFG.vmdq_conf_default->rx_adv_conf.vmdq_rx_conf.rx_mode =
-                ETH_VMDQ_ACCEPT_BROADCAST | ETH_VMDQ_ACCEPT_MULTICAST;
-
-            break;
-
-        case 0:
-            /* Enable/disable vm2vm comms. */
-            if (!strncmp(long_option[option_index].name, "vm2vm", MAX_LONG_OPT_SZ)) {
-                ret = parse_num_opt(optarg, (VM2VM_LAST - 1));
-                if (ret == -1) {
-                    RTE_LOG(INFO, VHOST_CONFIG,
-                            "Invalid argument for "
-                            "vm2vm [0|1|2]\n");
-                    us_vhost_usage(prgname);
-                    return -1;
-                } else {
-                    CFG.vm2vm_mode = (vm2vm_type)ret;
-                }
-            }
-
-            /* Enable/disable retries on RX. */
-            if (!strncmp(long_option[option_index].name, "rx-retry", MAX_LONG_OPT_SZ)) {
-                ret = parse_num_opt(optarg, 1);
-                if (ret == -1) {
-                    RTE_LOG(INFO, VHOST_CONFIG, "Invalid argument for rx-retry [0|1]\n");
-                    us_vhost_usage(prgname);
-                    return -1;
-                } else {
-                    CFG.enable_retry = ret;
-                }
-            }
-
-            /* Enable/disable TX checksum offload. */
-            if (!strncmp(long_option[option_index].name, "tx-csum", MAX_LONG_OPT_SZ)) {
-                ret = parse_num_opt(optarg, 1);
-                if (ret == -1) {
-                    RTE_LOG(INFO, VHOST_CONFIG, "Invalid argument for tx-csum [0|1]\n");
-                    us_vhost_usage(prgname);
-                    return -1;
-                } else
-                    CFG.enable_tx_csum = ret;
-            }
-
-            /* Enable/disable TSO offload. */
-            if (!strncmp(long_option[option_index].name, "tso", MAX_LONG_OPT_SZ)) {
-                ret = parse_num_opt(optarg, 1);
-                if (ret == -1) {
-                    RTE_LOG(INFO, VHOST_CONFIG, "Invalid argument for tso [0|1]\n");
-                    us_vhost_usage(prgname);
-                    return -1;
-                } else
-                    CFG.enable_tso = ret;
-            }
-
-            /* Specify the retries delay time (in useconds) on RX. */
-            if (!strncmp(long_option[option_index].name, "rx-retry-delay", MAX_LONG_OPT_SZ)) {
-                ret = parse_num_opt(optarg, INT32_MAX);
-                if (ret == -1) {
-                    RTE_LOG(INFO, VHOST_CONFIG, "Invalid argument for rx-retry-delay [0-N]\n");
-                    us_vhost_usage(prgname);
-                    return -1;
-                } else {
-                    CFG.burst_rx_delay_time = ret;
-                }
-            }
-
-            /* Specify the retries number on RX. */
-            if (!strncmp(long_option[option_index].name, "rx-retry-num", MAX_LONG_OPT_SZ)) {
-                ret = parse_num_opt(optarg, INT32_MAX);
-                if (ret == -1) {
-                    RTE_LOG(INFO, VHOST_CONFIG, "Invalid argument for rx-retry-num [0-N]\n");
-                    us_vhost_usage(prgname);
-                    return -1;
-                } else {
-                    CFG.burst_rx_retry_num = ret;
-                }
-            }
-
-            /* Enable/disable RX mergeable buffers. */
-            if (!strncmp(long_option[option_index].name, "mergeable", MAX_LONG_OPT_SZ)) {
-                ret = parse_num_opt(optarg, 1);
-                if (ret == -1) {
-                    RTE_LOG(INFO, VHOST_CONFIG, "Invalid argument for mergeable [0|1]\n");
-                    us_vhost_usage(prgname);
-                    return -1;
-                } else {
-                    CFG.mergeable = !!ret;
-                    if (ret) {
-                        CFG.vmdq_conf_default->rxmode.offloads |= DEV_RX_OFFLOAD_JUMBO_FRAME;
-                        CFG.vmdq_conf_default->rxmode.max_rx_pkt_len = JUMBO_FRAME_MAX_SIZE;
-                    }
-                }
-            }
-
-            /* Enable/disable stats. */
-            if (!strncmp(long_option[option_index].name, "stats", MAX_LONG_OPT_SZ)) {
-                ret = parse_num_opt(optarg, INT32_MAX);
-                if (ret == -1) {
-                    RTE_LOG(INFO, VHOST_CONFIG, "Invalid argument for stats [0..N]\n");
-                    us_vhost_usage(prgname);
-                    return -1;
-                } else {
-                    CFG.enable_stats = ret;
-                }
-            }
-
-            /* Set socket file path. */
-            if (!strncmp(long_option[option_index].name, "socket-file", MAX_LONG_OPT_SZ)) {
-                if (us_vhost_parse_socket_path(optarg) == -1) {
-                    RTE_LOG(INFO, VHOST_CONFIG, "Invalid argument for socket name (Max %d characters)\n", PATH_MAX);
-                    us_vhost_usage(prgname);
-                    return -1;
-                }
-            }
-
-            break;
-
-            /* Invalid option - print options. */
         default:
-            us_vhost_usage(prgname);
-            return -1;
+            fprintf(stderr, "Invalid option\n");
+            goto failed;
         }
     }
 
@@ -318,4 +366,8 @@ int us_vhost_parse_args(int argc, char **argv) {
     }
 
     return 0;
+
+failed:
+    us_vhost_usage(prgname);
+    return -1;
 }
