2d87d60cd246ab4b6f28cc7d0f5af31e5cee573e
Idhibhat Pankam <marginpankam@gmail.com>
2025-12-19 01:07:47 -0700

    spanwn

--- setup/vanilla/spawn_vms.sh
+++ setup/vanilla/spawn_vms.sh
@@ -2,14 +2,15 @@
 
 for i in {0..31}; do
     if [ $((i % 2)) -eq 0 ]; then
+        # server
         sudo cloud-hypervisor \
         --cpus boot=1 \
         --memory size=512M \
-        --kernel /tmp/vmlinux.bin \
+        --kernel /proj/faasnetworkstack-PG0/testing/kernels/vm$i-kernel.bin \
         --cmdline "console=ttyS0 console=hvc0 root=/dev/vda1 rw systemd.mask=systemd-networkd-wait-online.service systemd.mask=snapd.service systemd.mask=snapd.seeded.service systemd.mask=snapd.socket" \
-        --disk path=/tmp/vm0-img.raw path=/tmp/cloudinit/cloudinit-vm$i.img \
+        --disk path=/proj/faasnetworkstack-PG0/testing/images/vm$i-img.raw path=/tmp/cloudinit/cloudinit-vm$i.img \
         --net "tap=tap$i,mac=12:34:56:78:90:$(printf "%02X" $i)" 
     else
-        ./setup/setup_node.sh $i ens1f1np1
+        # client
     fi
 done
