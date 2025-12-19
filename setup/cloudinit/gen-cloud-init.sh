02b5dd44b9750900b3e456c03dce2513f2bfed59
Idhibhat Pankam <marginpankam@gmail.com>
2025-12-19 01:32:32 -0700

    gen network conf, rm multiququq

--- setup/cloudinit/gen-cloud-init.sh
+++ setup/cloudinit/gen-cloud-init.sh
@@ -4,27 +4,7 @@ set -ex
 # Get the directory where this script is located
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-rm -f "$SCRIPT_DIR/network-configs/network-vm"*
-mkdir -p "$SCRIPT_DIR/network-configs"
-for i in {0..31}; do
-  MAC_ADDRESS="12:34:56:78:90:$(printf "%02X" $i)"
-  cat > "$SCRIPT_DIR/network-configs/network-vm$i" <<EOF
-version: 2
-ethernets:
-  ens4:
-    # apply this configuration to whichever network interface has the MAC address
-    match:
-      macaddress: $MAC_ADDRESS
-    dhcp4: no
-    addresses: [192.168.100.$((i+2))/24]
-    routes:
-      - to: default
-        via: 192.168.100.1
-    nameservers:
-      addresses: [8.8.8.8, 8.8.4.4]
-    optional: true
-EOF
-done
+$SCRIPT_DIR/gen-network-config.sh
 
 # Function to create a cloud-init ISO
 create_iso() {
