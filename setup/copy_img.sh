a2bd91c3a5a9cca46b8e2ebec52fe0213f4ce44e
Idhibhat Pankam <marginpankam@gmail.com>
2025-12-19 01:25:06 -0700

    31

--- setup/copy_img.sh
+++ setup/copy_img.sh
@@ -4,7 +4,7 @@ DEST_DIR=/proj/faasnetworkstack-PG0/testing
 
 sudo rm -rf $DEST_DIR/kernels $DEST_DIR/images
 sudo mkdir -p $DEST_DIR/kernels $DEST_DIR/images
-for i in {0..10}; do
+for i in {0..31}; do
   (
     sudo cp /tmp/vmlinux.bin $DEST_DIR/kernels/vm$i-kernel.bin
     sudo cp /tmp/noble-server-cloudimg-amd64.raw $DEST_DIR/images/vm$i-img.raw
