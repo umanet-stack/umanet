3faeb2bd1d7974faf016cc818de15330ee255118
Idhibhat Pankam <marginpankam@gmail.com>
2025-12-19 00:50:45 -0700

    copy img

--- setup/copy_img.sh
+++ setup/copy_img.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
 
-sudo rm -rf /tmp/kernels /tmp/images
-mkdir -p /tmp/kernels /tmp/images
-for i in {0..31}; do
-  sudo cp --reflink=auto /tmp/vmlinux.bin /tmp/kernels/vm$i-kernel.bin &
-  sudo cp --reflink=auto /tmp/noble-server-cloudimg-amd64.raw /tmp/images/vm$i-img.raw &
+DEST_DIR=/proj/faasnetworkstack-PG0/testing
+
+sudo rm -rf $DEST_DIR/kernels $DEST_DIR/images
+sudo mkdir -p $DEST_DIR/kernels $DEST_DIR/images
+for i in {0..19}; do
+  (
+    sudo cp --reflink=auto /tmp/vmlinux.bin $DEST_DIR/kernels/vm$i-kernel.bin
+    sudo cp --reflink=auto /tmp/noble-server-cloudimg-amd64.raw $DEST_DIR/images/vm$i-img.raw
+    echo "✅ Copied vm$i-kernel.bin and vm$i-img.raw"
+  ) &
 done
 wait
