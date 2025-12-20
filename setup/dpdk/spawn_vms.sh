93f0b15df33b8548236b57a5073eeead16fb5a17
Idhibhat Pankam <marginpankam@gmail.com>
2025-12-20 03:32:45 -0700

    4096 queue

--- setup/dpdk/spawn_vms.sh
+++ setup/dpdk/spawn_vms.sh
@@ -36,9 +36,10 @@ spawn_vm() {
         --disk \
             path="$IMG_DIR/vm$i-img.raw" \
             path="$CLOUDINIT_DIR/cloudinit-vm$i.img" \
-        --net "mac=12:34:56:78:90:$(printf '%02X' $i),vhost_user=true,socket=/mnt/huge/sock$i,num_queues=2,vhost_mode=client,queue_size=2048" \
+        --net "mac=12:34:56:78:90:$(printf '%02X' $i),vhost_user=true,socket=/mnt/huge/sock$i,num_queues=2,vhost_mode=client,queue_size=4096" \
         > "$logfile" 2>&1 &
     
+    # queue_size sets both TX/RX's no. of descriptors, larger may reduce warnings of vhost pkt enqueue failures
     echo "  VM$i -> $logfile"
 }
 
