31cf3cc70e479a28f2819e48f83127831760ca76
Idhibhat Pankam <marginpankam@gmail.com>
2025-12-20 00:19:01 -0700

    fix sock

--- setup/dpdk/spawn_vms.sh
+++ setup/dpdk/spawn_vms.sh
@@ -36,7 +36,7 @@ spawn_vm() {
         --disk \
             path="$IMG_DIR/vm$i-img.raw" \
             path="$CLOUDINIT_DIR/cloudinit-vm$i.img" \
-        --net "mac=12:34:56:78:90:$(printf '%02X' $i),vhost_user=true,socket=/mnt/huge/sock0,num_queues=2,vhost_mode=client,queue_size=2048" \
+        --net "mac=12:34:56:78:90:$(printf '%02X' $i),vhost_user=true,socket=/mnt/huge/sock$i,num_queues=2,vhost_mode=client,queue_size=2048" \
         > "$logfile" 2>&1 &
     
     echo "  VM$i -> $logfile"
