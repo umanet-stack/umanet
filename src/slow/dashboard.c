5b92790aa60933f593e93f6bc17bad000466839a
Idhibhat Pankam <marginpankam@gmail.com>
2025-12-31 09:09:10 -0700

    basic dash

--- src/slow/dashboard.c
+++ src/slow/dashboard.c
@@ -1,3 +1,4 @@
+#include "src/include/state.h"
 #include <stdarg.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -32,9 +33,60 @@ void control_dashboard(int rx, int tx, int drops, int vms) {
     fprintf(tty_fp, "\033[2J"); // clear screen
     fprintf(tty_fp, "\033[H");  // move cursor to top-left (1;1)
 
-    fprintf(tty_fp, "RX: %d packets/sec\n", rx);
-    fprintf(tty_fp, "TX: %d packets/sec\n", tx);
-    fprintf(tty_fp, "Drops: %d | VMs: %d\n", drops, vms);
+    struct vdev_list *vdev_list_ptr = atomic_load(&vdev_list);
+    fprintf(tty_fp, "vhosts: %d\n", vdev_list_ptr->num);
+    for (int i = 0; i < global->vhost_rx_cores; i++) {
+        fprintf(tty_fp, "\nVHOST RX CORE %d: ", i);
+        struct vhost_rx_ctx *ctx = vhost_rx_ctxs[i];
+        struct vhost_plan *plan = atomic_load(&vhost_rx_plans[i]);
+        uint64_t pkt_count = 0;
+        uint64_t call_count = 0;
+        uint64_t empty_poll_count = 0;
+        uint64_t max_poll_count = 0;
+        uint64_t ring_enq_fail_count = 0;
+
+        for (int j = 0; j < plan->num; j++) {
+            fprintf(tty_fp, "%d ", plan->vids[j]);
+            pkt_count += ctx->vdev_stats[plan->vids[j]]->pkt_count;
+            call_count += ctx->vdev_stats[plan->vids[j]]->call_count;
+            empty_poll_count += ctx->vdev_stats[plan->vids[j]]->empty_poll_count;
+            max_poll_count += ctx->vdev_stats[plan->vids[j]]->max_poll_count;
+            ring_enq_fail_count += ctx->vdev_stats[plan->vids[j]]->ring_enq_fail_count;
+        }
+        fprintf(tty_fp, "\n");
+
+        fprintf(tty_fp, "pkt: %lu\t", pkt_count);
+        fprintf(tty_fp, "call: %lu\t", call_count);
+        fprintf(tty_fp, "empty_poll: %lu\t", empty_poll_count);
+        fprintf(tty_fp, "max_poll: %lu\t", max_poll_count);
+        fprintf(tty_fp, "ring_enq_fail: %lu\n", ring_enq_fail_count);
+    }
+    for (int i = 0; i < global->vhost_tx_cores; i++) {
+        fprintf(tty_fp, "\nVHOST TX CORE %d: ", i);
+        struct vhost_tx_ctx *ctx = vhost_tx_ctxs[i];
+        struct vhost_plan *plan = atomic_load(&vhost_tx_plans[i]);
+        uint64_t pkt_count = 0;
+        uint64_t call_count = 0;
+        uint64_t max_send_count = 0;
+        uint64_t send_fail_count = 0;
+        uint64_t ring_deq_max_count = 0;
+
+        for (int j = 0; j < plan->num; j++) {
+            fprintf(tty_fp, "%d ", plan->vids[j]);
+            pkt_count += ctx->vdev_stats[plan->vids[j]]->pkt_count;
+            call_count += ctx->vdev_stats[plan->vids[j]]->call_count;
+            max_send_count += ctx->vdev_stats[plan->vids[j]]->max_send_count;
+            send_fail_count += ctx->vdev_stats[plan->vids[j]]->send_fail_count;
+            ring_deq_max_count += ctx->vdev_stats[plan->vids[j]]->ring_deq_max_count;
+        }
+        fprintf(tty_fp, "\n");
+
+        fprintf(tty_fp, "pkt: %lu\t", pkt_count);
+        fprintf(tty_fp, "call: %lu\t", call_count);
+        fprintf(tty_fp, "max_send: %lu\t", max_send_count);
+        fprintf(tty_fp, "send_fail: %lu\t", send_fail_count);
+        fprintf(tty_fp, "ring_deq_max: %lu\n", ring_deq_max_count);
+    }
 
     fflush(tty_fp);
 }
