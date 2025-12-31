935b9813033f08d4beafb07c3c5425a498d854e6
Idhibhat Pankam <marginpankam@gmail.com>
2025-12-31 03:46:46 -0700

    eth tx

--- src/fast/eth_tx.c
+++ src/fast/eth_tx.c
@@ -8,37 +8,44 @@
 static inline int network_send(struct eth_tx_ctx *ctx, unsigned num, struct rte_mbuf **pkts);
 
 void eth_tx_loop(struct eth_tx_ctx *ctx) {
+    LOG_IMPT("[%u] Entering eth_tx loop...\n", ctx->core_id);
+
     while (1) {
         STATS_TS(start);
 #ifdef DEBUG
         sleep(1);
 #endif
 
-        struct rte_mbuf *pkts[MAX_PKT_BURST];
         uint16_t num = MAX_PKT_BURST;
+        struct rte_mbuf *pkts[num];
 
-        rte_ring_dequeue_burst(global->eth_tx_rings[ctx->eth_queue_id], (void **)pkts, num, NULL);
-        network_send(ctx, num, pkts);
+        int deq_num = rte_ring_dequeue_burst(global->eth_tx_rings[ctx->eth_queue_id], (void **)pkts, num, NULL);
+        if (deq_num == num) {
+            STATS_ADD(ctx->stats, ring_deq_max_count, 1);
+        }
+        LOG_INFO("[%d] Dequeued %d packets from eth_tx_ring[%d] to eth_tx_loop\n", ctx->core_id, deq_num,
+                 ctx->eth_queue_id);
+
+        if (deq_num > 0)
+            network_send(ctx, deq_num, pkts);
     }
 }
 
 static inline int network_send(struct eth_tx_ctx *ctx, unsigned num, struct rte_mbuf **pkts) {
-    uint16_t queued = rte_eth_tx_burst(global->eth_port_id, ctx->eth_queue_id, pkts, num);
-    if (queued == 0) {
-        // TX queue might be full - this could indicate transmission issues
-        LOG_WARN("[%d] TX queue full: 0/%u packets queued\n", ctx->core_id, num);
+    STATS_ADD(ctx->stats, call_count, 1);
+    int16_t ret = rte_eth_tx_burst(global->eth_port_id, ctx->eth_queue_id, pkts, num);
+    if (ret == 0) {
+        STATS_ADD(ctx->stats, send_fail_count, 1);
         return 0;
     }
 
-    if (queued < num) {
-        LOG_WARN("[%d] TX queue partial: %u/%u packets queued\n", ctx->core_id, queued, num);
-        // STATS_ADD(ctx, eth_tx_partial, 1); // Track partial sends
+    STATS_ADD(ctx->stats, pkt_count, ret);
+    if (ret == num) {
+        STATS_ADD(ctx->stats, max_send_count, 1);
     }
 
-    // STATS_ADD(ctx, pkt_eth_tx, queued);
-    // STATS_ADD(ctx, call_eth_tx, 1);
-    LOG_ETH_OUT("[%d] Sent %d packets to physical NIC\n", ctx->core_id, queued);
-    PRINT_PKTS(pkts, queued, LOG_ETH_OUT);
+    LOG_ETH_OUT("[%d] Sent %d packets to ETH queue %d\n", ctx->core_id, ret, ctx->eth_queue_id);
+    PRINT_PKTS(pkts, ret, LOG_ETH_OUT);
 
-    return queued;
+    return ret;
 }
