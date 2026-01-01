41bd8a85596a10e6d73b9fb9ebba7f83c561ae4b
Idhbhat Pankam <marginpankam@gmail.com>
2026-01-01 07:39:37 -0700

    e

--- src/include/state.h
+++ src/include/state.h
@@ -20,10 +20,10 @@
 // #define VHOST_RX_CORES 4
 
 // multinode server
-#define ETH_TX_CORES 2
-#define ETH_RX_CORES 2
-#define VHOST_TX_CORES 4
-#define VHOST_RX_CORES 2
+#define ETH_TX_CORES 1
+#define ETH_RX_CORES 3
+#define VHOST_TX_CORES 5
+#define VHOST_RX_CORES 1
 
 // #define ETH_TX_CORES 3
 // #define ETH_RX_CORES 2
