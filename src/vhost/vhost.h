1f1a72478c737678c0afda03d9682a34e8cad468
Idhibhat Pankam <marginpankam@gmail.com>
2026-01-02 08:59:47 -0700

    rm nat, deadcode

--- src/vhost/vhost.h
+++ src/vhost/vhost.h
@@ -15,15 +15,9 @@
 
 enum { VIRTIO_RXQ, VIRTIO_TXQ };
 
-#define REQUEST_DEV_REMOVAL 1
-#define ACK_DEV_REMOVAL 0
-
 #define DEVICE_MAC_LEARNING 0
 #define DEVICE_RX 1
 #define DEVICE_SAFE_REMOVE 2
-
-#define BURST_TX_DRAIN_US 100 /* TX drain every ~100us */
-#define MBUF_TABLE_DRAIN_TSC ((rte_get_tsc_hz() + US_PER_S - 1) / US_PER_S * BURST_TX_DRAIN_US)
 
 extern const struct rte_vhost_device_ops virtio_net_device_ops;
 extern struct route_table route_table;
@@ -81,29 +75,6 @@ struct route_table {
     struct rte_hash *ip_2_vid;
 };
 
-// struct vhost_dev { // vhost device
-//     // Device MAC address (Obtained on first TX packet).
-//     struct rte_ether_addr mac_address;
-//     uint32_t vm_ip_address;
-//     /**< A device is set as ready if the MAC address has been set. */
-//     volatile uint8_t ready;
-//     /**< Device is marked for removal from the data core. */
-//     volatile uint8_t remove;
-
-//     int vid;                      // vhost device ID, assigned by dpdk
-//     uint64_t features;            // Virtio feature flags
-//     size_t hdr_len;               // Header length
-//     struct rte_vhost_memory *mem; // Guest memory mapping
-
-//     // Rate-limited logging for failed enqueue attempts
-//     uint64_t last_failed_log_ts; // TSC timestamp of last log
-//     uint64_t failed_pkts_count;  // Cumulative failed packets since last log
-
-//     // Track consecutive empty polls before marking device inactive
-//     uint8_t empty_poll_count;
-//     uint8_t is_active;
-// } __rte_cache_aligned;
-
 int check_device_state(struct vhost_dev *vdev, const char *func);
 void unregister_vhost_drivers(int socket_num, const char *path);
 int register_vhost_drivers();
@@ -124,22 +95,6 @@ int remove_route_entry(int vid, struct rte_ether_addr *mac, uint32_t ip);
 // use vid to get vdev by indexing the vdev_list global variable
 int find_vid_by_mac(struct rte_ether_addr *mac);
 int find_vid_by_ip(uint32_t ip);
-
-// static inline unsigned is_route_added()
-
-// static inline unsigned vhost_send(struct dataplane_context *ctx, unsigned num, unsigned vid, struct rte_mbuf **pkts)
-// {
-//     num = rte_vhost_enqueue_burst(vid, VIRTIO_RXQ, pkts, num);
-//     if (num == 0)
-//         return 0;
-
-//     STATS_ADD(ctx, pkt_vhost_tx, num);
-//     STATS_ADD(ctx, call_vhost_tx, 1);
-//     LOG_VM_OUT("[%d](%d) Sent %d packets to VM\n", ctx->id, vid, num);
-//     PRINT_PKTS(pkts, num, LOG_VM_OUT);
-
-//     return num;
-// }
 
 #define PERTHREAD_MBUFS 8192
 #define BUFFER_SIZE 2048
