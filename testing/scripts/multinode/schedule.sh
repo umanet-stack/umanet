1b378f27f8a19737d6983d9583f3b03c57252814
Pannawich Lohanimit <30285202+plohan@users.noreply.github.com>
2026-01-08 01:28:41 +0700

    a

--- testing/scripts/multinode/schedule.sh
+++ testing/scripts/multinode/schedule.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+set -e
+
 source env.sh
+
+[ command -v at ] || echo "at is not installed plase run 'sudo apt install -y at'"
 
 if [ "$NODE_ID" = "0" ]; then
   ./testing/scripts/multinode/schedule_client.sh
