f30347b0662e71c0bcc2d18e2b9c35ae47c4db0f
Pannawich Lohanimit <30285202+plohan@users.noreply.github.com>
2026-01-09 00:34:22 +0700

    a

--- testing/scripts/multinode/run_one_client.sh
+++ testing/scripts/multinode/run_one_client.sh
@@ -3,4 +3,4 @@
 ./setup/vm/setup_br_tap.sh $1
 ./setup/vm/spawn_vms.sh tap $1 vm-client
 python testing/process_logs/main.py tap vm-client
-ps aux | grep cloud-hypervisor | grep -v grep | awk '{print \$2}' | xargs kill -9
+ps aux | grep cloud-hypervisor | grep -v grep | awk '{print $2}' | xargs sudo kill -9
--- testing/scripts/multinode/run_one_server.sh
+++ testing/scripts/multinode/run_one_server.sh
@@ -3,4 +3,4 @@
 ./setup/vm/setup_br_tap.sh $1
 ./setup/vm/spawn_vms.sh tap $1 vm-server
 
-ps aux | grep cloud-hypervisor | grep -v grep | awk '{print \$2}' | xargs kill -9
+ps aux | grep cloud-hypervisor | grep -v grep | awk '{print $2}' | xargs sudo kill -9
