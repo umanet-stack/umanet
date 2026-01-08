a4ddb4c2fd3e671f8ed3f8758b6d95913c5cff5e
Idhibhat Pankam <marginpankam@gmail.com>
2026-01-08 09:11:23 -0700

    comment logs

--- src/fast/vhost_rx.c
+++ src/fast/vhost_rx.c
@@ -306,15 +306,12 @@ static inline unsigned vhost_poll(struct vhost_rx_ctx *ctx, unsigned num, unsign
         STATS_ADD(ctx->vdev_stats[vid], max_poll_count, 1);
     }
 
-    static int count = 0;
-    if (count < 500) {
-        for (int i = 0; i < ret; i++) {
-            LOG_IMPT("VHOST RX: pkt %d: nb_segs=%u pkt_len=%u\n", i, pkts[i]->nb_segs, pkts[i]->pkt_len);
-        }
-        count++;
-    }
-    // for (int i = 0; i < ret; i++) {
-    //     printf("VHOST RX pkt %d: nb_segs=%u pkt_len=%u\n", i, pkts[i]->nb_segs, pkts[i]->pkt_len);
+    // static int count = 0;
+    // if (count < 500) {
+    //     for (int i = 0; i < ret; i++) {
+    //         LOG_IMPT("VHOST RX: pkt %d: nb_segs=%u pkt_len=%u\n", i, pkts[i]->nb_segs, pkts[i]->pkt_len);
+    //     }
+    //     count++;
     // }
 
     LOG_VM_IN("[%d](%d) Received %d packets from VM\n", ctx->core_id, vid, ret);
--- src/network/network.h
+++ src/network/network.h
@@ -44,8 +44,7 @@ static inline void free_pkts(struct rte_mbuf **pkts, uint16_t n) {
         rte_pktmbuf_free(pkts[n]);
 }
 
-// #define PERTHREAD_MBUFS 8191
-#define PERTHREAD_MBUFS 32767 // Increased for MTU 9000 multi-segment packets
+#define PERTHREAD_MBUFS 32767
 #define MBUF_SIZE 9728
 
 static inline struct rte_mempool *mempool_alloc(const char *name) {
@@ -56,9 +55,6 @@ static inline struct rte_mempool *mempool_alloc(const char *name) {
     snprintf(pool_name, sizeof(pool_name), "%s_%u", name, n);
 
     struct rte_mempool *mp = rte_pktmbuf_pool_create(pool_name, PERTHREAD_MBUFS, 256, 0, MBUF_SIZE, rte_socket_id());
-    // struct rte_mempool *mp =
-    // rte_pktmbuf_pool_create(name, PERTHREAD_MBUFS, MBUF_SIZE, 32, sizeof(struct rte_pktmbuf_pool_private),
-    //                        rte_pktmbuf_pool_init, NULL, rte_pktmbuf_init, NULL, rte_socket_id(), 0);
 
     if (mp == NULL) {
         LOG_ERROR("Failed to create mempool %s: %s\n", name, rte_strerror(rte_errno));
