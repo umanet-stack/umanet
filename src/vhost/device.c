5c4a708a5aa80b7516aa838c2b01b5ddfcc7ec9e
Idhibhat Pankam <marginpankam@gmail.com>
2026-01-07 21:22:01 -0700

    log negotiated vhost feat

--- src/vhost/device.c
+++ src/vhost/device.c
@@ -163,16 +163,31 @@ static int new_device(int vid) {
     rte_vhost_enable_guest_notification(vid, VIRTIO_TXQ, 0);
 
     // Check negotiated protocol features after device connection
-    // uint64_t proto_features;
-    // if (rte_vhost_get_negotiated_protocol_features(vid, &proto_features) == 0) {
-    //     LOG_INFO("(%d) Negotiated protocol features: 0x%lx\n", vid, proto_features);
-    //     if (proto_features & (1ULL << VHOST_USER_PROTOCOL_F_INFLIGHT_SHMFD)) {
-    //         LOG_INFO("(%d) Zero-copy enabled: INFLIGHT_SHMFD protocol feature negotiated\n", vid);
-    //     } else {
-    //         LOG_WARN("(%d) Zero-copy NOT possible: missing INFLIGHT_SHMFD (protocol features: 0x%lx)\n", vid,
-    //                  proto_features);
-    //     }
-    // }
+    uint64_t proto_features;
+    if (rte_vhost_get_negotiated_protocol_features(vid, &proto_features) == 0) {
+        LOG_INFO("(%d) Negotiated protocol features: 0x%lx\n", vid, proto_features);
+        if (proto_features & (1ULL << VHOST_USER_PROTOCOL_F_INFLIGHT_SHMFD)) {
+            LOG_INFO("(%d) Zero-copy enabled: INFLIGHT_SHMFD protocol feature negotiated\n", vid);
+        }
+        if (proto_features & (1ULL << VIRTIO_NET_F_MTU)) {
+            LOG_INFO("(%d) MTU enabled: MTU protocol feature negotiated\n", vid);
+        }
+        if (proto_features & (1ULL << VIRTIO_NET_F_HOST_TSO4)) {
+            LOG_INFO("(%d) TSO4 enabled: TSO4 protocol feature negotiated\n", vid);
+        }
+        if (proto_features & (1ULL << VIRTIO_NET_F_HOST_TSO6)) {
+            LOG_INFO("(%d) TSO6 enabled: TSO6 protocol feature negotiated\n", vid);
+        }
+        if (proto_features & (1ULL << VIRTIO_NET_F_GUEST_TSO4)) {
+            LOG_INFO("(%d) TSO4 enabled: TSO4 protocol feature negotiated\n", vid);
+        }
+        if (proto_features & (1ULL << VIRTIO_NET_F_GUEST_TSO6)) {
+            LOG_INFO("(%d) TSO6 enabled: TSO6 protocol feature negotiated\n", vid);
+        }
+        if (proto_features & (1ULL << VIRTIO_NET_F_GUEST_ECN)) {
+            LOG_INFO("(%d) ECN enabled: ECN protocol feature negotiated\n", vid);
+        }
+    }
 
     int res = vhost_rx_plan_add(vid);
     if (res != 0) {
