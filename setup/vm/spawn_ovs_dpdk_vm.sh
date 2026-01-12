55ac7b2e423cf51b8651a4ffbcf7618182c5ae79
Idhibhat Pankam <marginpankam@gmail.com>
2026-01-12 03:40:46 -0700

    OVS_VM_SIZE

--- setup/vm/spawn_ovs_dpdk_vm.sh
+++ setup/vm/spawn_ovs_dpdk_vm.sh
@@ -10,6 +10,11 @@ if [ "$#" -ne 3 ]; then
     exit 1
 fi
 
+if [ "$OVS_VM_SIZE" != "microvm" ] && [ "$OVS_VM_SIZE" != "large" ]; then
+    echo "Error: OVS_VM_SIZE must be microvm or large"
+    exit 1
+fi
+
 i=$1
 ROLE=$2
 COMMAND=$3
@@ -20,15 +25,25 @@ logfile="$LOG_DIR/vm$i.log"
 # Base64 encode the command to avoid space issues in kernel cmdline
 TEST_COMMAND_B64=$(echo -n "$COMMAND" | base64 -w 0)
 
+if [ "$OVS_VM_SIZE" = "microvm" ]; then
+    CPU="1"
+    MEM="512M"
+    QUEUES="2"
+elif [ "$OVS_VM_SIZE" = "large" ]; then
+    CPU="8"
+    MEM="2048M"
+    QUEUES="8"
+fi
+
 sudo systemd-run --scope --slice=vms.slice \
 cloud-hypervisor \
-    --cpus boot=1 \
-    --memory size=512M,hugepages=on,shared=on,prefault=on \
+    --cpus boot=$CPU \
+    --memory size=$MEM,hugepages=on,shared=on,prefault=on \
     --kernel "$TMPDIR/vmlinux.bin" \
     --initramfs /tmp/initramfs-overlay.img \
     --cmdline "console=ttyS0 console=hvc0 rdinit=/init ROLE=$ROLE TEST_COMMAND_B64=$TEST_COMMAND_B64" \
     --disk path="$TMPDIR/vm-img.raw",readonly=on path="$TMPDIR/disks/state-$i.img" path="$TMPDIR/cloudinit/cloudinit-vm$i.img" \
-    --net "mac=${NODE_ID}2:34:56:78:92:$(printf '%02X' $i),vhost_user=on,socket=/usr/local/var/run/openvswitch/vhost-user$i,num_queues=2,vhost_mode=client,queue_size=4096" \
+    --net "mac=${NODE_ID}2:34:56:78:92:$(printf '%02X' $i),vhost_user=on,socket=/usr/local/var/run/openvswitch/vhost-user$i,num_queues=$QUEUES,vhost_mode=client,queue_size=4096" \
     > "$logfile" 2>&1 &
 
 echo "  VM$i -> $COMMAND"
