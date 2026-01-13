9d02604e60179513a59883993f55b98ec4fb5484
Idhibhat Pankam <marginpankam@gmail.com>
2026-01-12 21:38:29 -0700

    append

--- testing/multinode/vm-vm/run_all.sh
+++ testing/multinode/vm-vm/run_all.sh
@@ -17,26 +17,26 @@ elif [ "$TEST" = "sockperf" ]; then
 fi
 
 ssh -i ~/.ssh/cloudlab "$USER@$NODE1" "cd $BASE_DIR && bash -s" < ${MULTINODE_DIR}/init_node.sh \
-  "$BASE_DIR" "$NETWORK" &> "$MULTINODE_DIR/node1.log"
+  "$BASE_DIR" "$NETWORK" &>> "$MULTINODE_DIR/node1.log"
 log "⭐️ node 1 initialized"
 
-${MULTINODE_DIR}/init_node.sh "$BASE_DIR" "$NETWORK" &> "$MULTINODE_DIR/node0.log"
+${MULTINODE_DIR}/init_node.sh "$BASE_DIR" "$NETWORK" &>> "$MULTINODE_DIR/node0.log"
 log "⭐️ node 0 initialized"
 
-log "======= TEST: $TEST_CMD ======="
+log "======= TEST: $TEST_CMD (network: $NETWORK) ======="
 for VMS in $(seq 1 1); do
-  log "Running test: $VMS VMs (network: $NETWORK)"
+  log "Running test: $VMS VMs"
 
   OUTDIR="$BASE_OUT/vms_$VMS"
 
   ssh -i ~/.ssh/cloudlab "$USER@$NODE1" "cd $BASE_DIR && bash -s" < ${MULTINODE_DIR}/run_node1.sh \
-    "$BASE_DIR" "$NETWORK" "$VMS" &> "$MULTINODE_DIR/node1.log"
+    "$BASE_DIR" "$NETWORK" "$VMS" &>> "$MULTINODE_DIR/node1.log"
   log "  started $VMS VMs on node 1"
   sleep 5
 
-  ${MULTINODE_DIR}/run_node0.sh "$BASE_DIR" "$NETWORK" "$VMS" &> "$MULTINODE_DIR/node0.log"
+  ${MULTINODE_DIR}/run_node0.sh "$BASE_DIR" "$NETWORK" "$VMS" &>> "$MULTINODE_DIR/node0.log"
   log "  started $VMS VMs on node 0" 
   sleep 45
 
-  python testing/process_logs/main.py $NETWORK vm-client
+  python testing/process_logs/main.py $NETWORK vm-client &>> "$REPORT_LOG"
 done
