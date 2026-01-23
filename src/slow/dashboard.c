7c2856130d54203f1b6a60400bbcee05df1b838f
Idhibhat Pankam <marginpankam@gmail.com>
2026-01-23 08:16:30 -0700

    dash

--- src/slow/dashboard.c
+++ src/slow/dashboard.c
@@ -27,28 +27,28 @@ void control_dashboard(int rx, int tx, int drops, int vms) {
     fprintf(tty_fp, "\033[2J"); // clear screen
     fprintf(tty_fp, "\033[H");  // move cursor to top-left (1;1)
 
-    for (int i = 0; i < config.eth_rx_cores; i++) {
+    for (int i = 0; i < global->fp_cores; i++) {
         fprintf(tty_fp, CYAN_PREFIX "ETH RX CORE %d: " RESET_COLOR, i);
-        fprintf(tty_fp, "pkt: %s\t", display_number(eth_rx_ctxs[i]->stats->pkt_count));
-        fprintf(tty_fp, "call: %s\t", display_number(eth_rx_ctxs[i]->stats->call_count));
-        fprintf(tty_fp, "empty_poll: %s\t", display_number(eth_rx_ctxs[i]->stats->empty_poll_count));
-        fprintf(tty_fp, "max_poll: %s\t", display_number(eth_rx_ctxs[i]->stats->max_poll_count));
-        fprintf(tty_fp, "ring_enq_fail: %s\n", display_number(eth_rx_ctxs[i]->stats->ring_enq_fail_count));
+        fprintf(tty_fp, "pkt: %s\t", display_number(fp_ctxs[i]->eth_rx_ctx->stats->pkt_count));
+        fprintf(tty_fp, "call: %s\t", display_number(fp_ctxs[i]->eth_rx_ctx->stats->call_count));
+        fprintf(tty_fp, "empty_poll: %s\t", display_number(fp_ctxs[i]->eth_rx_ctx->stats->empty_poll_count));
+        fprintf(tty_fp, "max_poll: %s\t", display_number(fp_ctxs[i]->eth_rx_ctx->stats->max_poll_count));
+        fprintf(tty_fp, "ring_enq_fail: %s\n", display_number(fp_ctxs[i]->eth_rx_ctx->stats->ring_enq_fail_count));
     }
-    for (int i = 0; i < config.eth_tx_cores; i++) {
+    for (int i = 0; i < global->fp_cores; i++) {
         fprintf(tty_fp, GREEN_PREFIX "ETH TX CORE %d: " RESET_COLOR, i);
-        fprintf(tty_fp, "pkt: %s\t", display_number(eth_tx_ctxs[i]->stats->pkt_count));
-        fprintf(tty_fp, "call: %s\t", display_number(eth_tx_ctxs[i]->stats->call_count));
-        fprintf(tty_fp, "max_send: %s\t", display_number(eth_tx_ctxs[i]->stats->max_send_count));
-        fprintf(tty_fp, "requeue_pkt: %s\t", display_number(eth_tx_ctxs[i]->stats->requeue_pkt_count));
-        fprintf(tty_fp, "ring_deq_max: %s\n", display_number(eth_tx_ctxs[i]->stats->ring_deq_max_count));
+        fprintf(tty_fp, "pkt: %s\t", display_number(fp_ctxs[i]->eth_tx_ctx->stats->pkt_count));
+        fprintf(tty_fp, "call: %s\t", display_number(fp_ctxs[i]->eth_tx_ctx->stats->call_count));
+        fprintf(tty_fp, "max_send: %s\t", display_number(fp_ctxs[i]->eth_tx_ctx->stats->max_send_count));
+        fprintf(tty_fp, "requeue_pkt: %s\t", display_number(fp_ctxs[i]->eth_tx_ctx->stats->requeue_pkt_count));
+        fprintf(tty_fp, "ring_deq_max: %s\n", display_number(fp_ctxs[i]->eth_tx_ctx->stats->ring_deq_max_count));
     }
 
     struct vdev_list *vdev_list_ptr = atomic_load(&vdev_list);
     // fprintf(tty_fp, "vhosts: %d\n", vdev_list_ptr->num);
     fprintf(tty_fp, "\npkt\tcall\tempty_poll\tmax_poll\tvr_enq_fail\ter_enq_fail\n");
-    for (int i = 0; i < config.vhost_rx_cores; i++) {
-        struct vhost_rx_ctx *ctx = vhost_rx_ctxs[i];
+    for (int i = 0; i < global->fp_cores; i++) {
+        struct vhost_rx_ctx *ctx = fp_ctxs[i]->vhost_rx_ctx;
         struct vhost_plan *plan = atomic_load(&vhost_rx_plans[i]);
         fprintf(tty_fp, BLUE_PREFIX "VHOST RX CORE %d (%u): " RESET_COLOR, i, plan->num);
         uint64_t pkt_count = 0;
@@ -88,8 +88,8 @@ void control_dashboard(int rx, int tx, int drops, int vms) {
     }
 
     fprintf(tty_fp, "\npkt\tcall\tmax_send\trequeue_pkt\tring_deq_max\n");
-    for (int i = 0; i < config.vhost_tx_cores; i++) {
-        struct vhost_tx_ctx *ctx = vhost_tx_ctxs[i];
+    for (int i = 0; i < global->fp_cores; i++) {
+        struct vhost_tx_ctx *ctx = fp_ctxs[i]->vhost_tx_ctx;
         struct vhost_plan *plan = atomic_load(&vhost_tx_plans[i]);
         fprintf(tty_fp, MAGENTA_PREFIX "VHOST TX CORE %d (%u): " RESET_COLOR, i, plan->num);
         uint64_t pkt_count = 0;
